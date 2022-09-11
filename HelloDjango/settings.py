f3bc6ac9473fac28bc625882692c97a70e87895f
dianaGera <dianamatkava@gmail.com>
2022-09-11 12:46:50 +0400

    v

--- HelloDjango/settings.py
+++ HelloDjango/settings.py
@@ -14,6 +14,7 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 import django_heroku
+from distutils.util import strtobool
 
 load_dotenv() 
 
@@ -28,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = strtobool(os.getenv('DEBUG'))
 
 ALLOWED_HOSTS = ['127.0.0.1', 'cheat-django.herokuapp.com']
 
@@ -85,6 +86,7 @@ WSGI_APPLICATION = 'HelloDjango.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
 
 DATABASES = {
     'default': {
